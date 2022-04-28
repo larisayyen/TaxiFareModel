9f37b17447b6f9bea6a938fe1a270d194dd34aba
larisa yen <larisayyen@yandex.com>
2022-04-28 14:37:52 +0800

    updated fastapi

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,5 @@
 
-from turtle import pd
+#from turtle import pd
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
@@ -24,38 +24,39 @@ app.add_middleware(
 def index():
     return {"greeting": "Hello world"}
 
-# @app.get("/predict")
-# def make_predict(pickup_datetime,
-#                  pickup_longitude,
-#                  pickup_latitude,
-#                  dropoff_longitude,
-#                  dropoff_latitude,
-#                  passenger_count):
+@app.get("/predict")
+def make_predict(pickup_datetime,
+                 pickup_longitude,
+                 pickup_latitude,
+                 dropoff_longitude,
+                 dropoff_latitude,
+                 passenger_count):
 
-#     # create a datetime object from the user provided datetime
-#     pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
+    # create a datetime object from the user provided datetime
+    pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
 
-#     # localize the user datetime with NYC timezone
-#     eastern = pytz.timezone("US/Eastern")
-#     localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+    # localize the user datetime with NYC timezone
+    eastern = pytz.timezone("US/Eastern")
+    localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
 
-#     # localize the datetime to UTC
-#     utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+    # localize the datetime to UTC
+    utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
 
-#     formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
+    formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
 
-#     X_pred = pd.DataFrame.from_dict({
-#                 "key": formatted_pickup_datetime,
-#                 "pickup_datetime": formatted_pickup_datetime,
-#                 "pickup_longitude": pickup_longitude,
-#                 "pickup_latitude": pickup_latitude,
-#                 "dropoff_longitude": dropoff_longitude,
-#                 "dropoff_latitude": dropoff_latitude,
-#                 "passenger_count": passenger_count
-#             })
+    X_pred = pd.DataFrame({
+                "key": formatted_pickup_datetime,
+                "pickup_datetime": formatted_pickup_datetime,
+                "pickup_longitude": float(pickup_longitude),
+                "pickup_latitude": float(pickup_latitude),
+                "dropoff_longitude": float(dropoff_longitude),
+                "dropoff_latitude": float(dropoff_latitude),
+                "passenger_count": int(passenger_count)
+            },index=[1])
 
-#     pipeline = joblib.load(PATH_TO_LOCAL_MODEL)
+    pipeline = joblib.load(PATH_TO_LOCAL_MODEL)
 
-#     y_pred = pipeline.predict(X_pred)
+    y_pred = pipeline.predict(X_pred)
+    print(y_pred)
 
-#     return y_pred
+    return {'key':float(y_pred[0])}
